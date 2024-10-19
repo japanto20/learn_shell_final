457ca8c98d2b8caee3e73b4e2e536aa58da6bf85
Anto <japanto20@gmail.com>
2024-10-19 13:07:55 -0500

    frontend checking

--- common.sh
+++ common.sh
@@ -6,22 +6,34 @@ rm-f $log_file
 app_prerequisites() {
   print_heading "Add app user"
   useradd roboshop &>>$log_file
-  echo $?
+  if [ $? -eq 0 ]; then
+    echo -e "\e[32m SUCCESS \e[0m"
+  else
+    echo -e "\e[32m FAILURE \e[0m"
 
   print_heading "Create App Directory"
   rm -rf /app &>>$log_file
   mkdir /app &>>$log_file
-  echo $?
+  if [ $? -eq 0 ]; then
+    echo -e "\e[32m SUCCESS \e[0m"
+  else
+    echo -e "\e[32m FAILURE \e[0m"
 
   print_heading "Download App Content"
   curl -L -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip &>>$log_file
-  echo $?
+  if [ $? -eq 0 ]; then
+    echo -e "\e[32m SUCCESS \e[0m"
+  else
+    echo -e "\e[32m FAILURE \e[0m"
 
   cd /app
 
   print_heading "Extract App content"
   unzip /tmp/$app_name.zip &>>$log_file
-  echo $?
+  if [ $? -eq 0 ]; then
+    echo -e "\e[32m SUCCESS \e[0m"
+  else
+    echo -e "\e[32m FAILURE \e[0m"
 }
 
 print_heading() {
--- dispatch.sh
+++ dispatch.sh
@@ -4,11 +4,17 @@ app_name=dispatch
 
 print_heading "Copy Dispatch Service File"
 cp dispatch.service /etc/systemd/system/dispatch.service &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m SUCCESS \e[0m"
+else
+  echo -e "\e[32m FAILURE \e[0m"
 
 print_heading "Copy Install golang r"
 dnf install golang -y &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m SUCCESS \e[0m"
+else
+  echo -e "\e[32m FAILURE \e[0m"
 
 app_prerequisites
 
@@ -16,10 +22,16 @@ print_heading "Download App Dependencies"
 go mod init dispatch &>>$log_file
 go get &>>$log_file
 go build &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m SUCCESS \e[0m"
+else
+  echo -e "\e[32m FAILURE \e[0m"
 
 print_heading "Start Application Service"
 systemctl daemon-reload &>>$log_file
 systemctl enable dispatch &>>$log_file
 systemctl start dispatch &>>$log_file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32m SUCCESS \e[0m"
+else
+  echo -e "\e[32m FAILURE \e[0m"
