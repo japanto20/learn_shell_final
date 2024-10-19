f1dd766b49dfbb9a0964096c080e140e7ac28997
Anto <japanto20@gmail.com>
2024-10-19 10:17:50 -0500

    frontend checking

--- common.sh
+++ common.sh
@@ -1,23 +1,25 @@
 color="\e[36m"
 no_color="\e[0m"
+log_file=/tmp/roboshop.log
+rm-f $log_file
 
 app_prerequisites() {
   echo -e "$color Add app user $no_color"
-  useradd roboshop
+  useradd roboshop &>>$log_file
   echo $?
 
   echo -e "$color Create App Directory $no_color"
-  rm -rf /app
-  mkdir /app
+  rm -rf /app &>>$log_file
+  mkdir /app &>>$log_file
   echo $?
 
   echo -e "$color Download App Content $no_color"
-  curl -L -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip
+  curl -L -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip &>>$log_file
   echo $?
 
   cd /app
 
   echo -e "$color Extract App content $no_color"
-  unzip /tmp/$app_name.zip
+  unzip /tmp/$app_name.zip &>>$log_file
   echo $?
 }
