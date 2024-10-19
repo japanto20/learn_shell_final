aeb35c6e65e0089e580c03a5813d350cbb6e82bf
Anto <japanto20@gmail.com>
2024-10-19 09:46:09 -0500

    frontend checking

--- dispatch.sh
+++ dispatch.sh
@@ -3,11 +3,11 @@ app_name=dispatch
 
 echo -e "$color Copy Dispatch Service File $no_color"
 cp dispatch.service /etc/systemd/system/dispatch.service
-echo$?
+echo $?
 
 echo -e "$color Copy Install golang $no_color"
 dnf install golang -y
-echo$?
+echo $?
 
 app_prerequistes
 
@@ -15,10 +15,10 @@ echo -e "$color Download App Dependencies $no_color"
 go mod init dispatch
 go get
 go build
-echo$?
+echo $?
 
 echo -e "$color Start Application Service $no_color"
 systemctl daemon-reload
 systemctl enable dispatch
 systemctl start dispatch
-echo$?
+echo $?
